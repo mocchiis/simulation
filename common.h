1173ef08775f33b7c279e2a9a3018ffbf094de4e
mocchiis <45111449+mocchiis@users.noreply.github.com>
2019-06-25 19:46:20 +0900

    Create common.h

---
+++ common.h
@@ -0,0 +1,8 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+static const char *DATA_DIRECTORY = "C://graphics/resource/";
+static const char *SHADER_DIRECTORY = "C://graphics/libs/class/shaders/";
+
+
+#endif  // _COMMON_H_
