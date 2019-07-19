33d77c66e5bf21c3a3c1a055fe21c659b1faf1af
mocchiis <45111449+mocchiis@users.noreply.github.com>
2019-07-19 12:25:22 +0900

    Create common.h

---
+++ 拡散視覚化/common.h
@@ -0,0 +1,13 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+static const char *DATA_DIRECTORY = "C://graphics/resource/";
+//static const char *SHADER_DIRECTORY = "C://graphics/src/final/Diffusion/shaders";
+static const char *SHADER_DIRECTORY = "C://graphics/src/diffusion_visualization/shaders/";
+
+
+//static const char *SHADER_DIRECTORY = "C://graphics/libs/class/shaders/";
+static const char *SHADER_DIRECTORY2 = "C://graphics/src/Day4/shader/";
+
+
+#endif  // _COMMON_H_
